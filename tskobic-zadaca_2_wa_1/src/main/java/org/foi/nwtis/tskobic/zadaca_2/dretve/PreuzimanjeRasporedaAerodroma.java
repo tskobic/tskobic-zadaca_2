0bb5fad802e8a28816d06a286855245f6ae950b1
Toni Škobić <73468642+tskobic@users.noreply.github.com>
2022-04-26 16:12:33 +0000

    26.4. 2. zadaća home setup

--- tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/dretve/PreuzimanjeRasporedaAerodroma.java
+++ tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/dretve/PreuzimanjeRasporedaAerodroma.java
@@ -17,81 +17,86 @@ public class PreuzimanjeRasporedaAerodroma extends Thread {
 	long vrijemePauza;
 	long vrijemeCiklusa;
 	long vrijemeObrade;
-    String korisnik;
-    String lozinka;
-    OSKlijent oSKlijent ;
-	
+	String korisnik;
+	String lozinka;
+	OSKlijent oSKlijent;
+
 	@Override
 	public synchronized void start() {
 		// TODO preuzmi iz konfiguracijskih podataka
-		preuzimanjeOd = 1648764000; 
+		preuzimanjeOd = 1648764000;
 		preuzimanjeDo = 1648850400;
 		preuzimanjeVrijeme = 6 * 60 * 60;
 		vrijemePauza = 20;
 		vrijemeCiklusa = 300 * 1000;
 		vrijemeObrade = preuzimanjeOd;
-		
+
 		korisnik = "xxxx";
 		lozinka = "yyyy";
+
 		oSKlijent = new OSKlijent(korisnik, lozinka);
-		
+
 		super.start();
 	}
 
 	@Override
 	public void run() {
-		while(vrijemeObrade < preuzimanjeDo) {
+		while (vrijemeObrade < preuzimanjeDo) {
 			// TODO preuzmi aerodrome iz tablice AERODROMI_PRACENI
-            List<Aerodrom> aerodromi = new ArrayList<>();
-            Aerodrom ad = new Aerodrom("LDZA", "Airport Zagreb", "HR", new Lokacija("0", "0"));         
-            aerodromi.add(ad);         
-            ad = new Aerodrom("LDVA", "Airport Varaždin", "HR", new Lokacija("0", "0"));         
-            aerodromi.add(ad);          
-            ad = new Aerodrom("EDDF", "Airport Frankfurt", "DE", new Lokacija("0", "0"));       
-            aerodromi.add(ad);          
-            ad = new Aerodrom("EDDB", "Airport Berlin", "DE", new Lokacija("0", "0"));       
-            aerodromi.add(ad);           
-            ad = new Aerodrom("LOWW", "Airport Vienna", "AT", new Lokacija("0", "0"));
-            aerodromi.add(ad);
-            
-            List<AvionLeti> avioniPolasci;
-    		try {
-    			avioniPolasci = oSKlijent.getDepartures(ad.getIcao(), vrijemeObrade, vrijemeObrade + preuzimanjeVrijeme);
-    	        if (avioniPolasci != null) {
-    	            System.out.println("Broj letova: " + avioniPolasci.size());
-    	            //TODO upiši podatke u tablicu AERODROMI_POLASCI
-    	            for(AvionLeti a : avioniPolasci) {
-    	                System.out.println("Avion: " + a.getIcao24() + " Odredište: " + a.getEstArrivalAirport());
-    	            }
-    	        }
-    		} catch (NwtisRestIznimka e) {
-    			e.printStackTrace();
-    		}
-            System.out.println("Dolasci na aerodrom: " + ad.getIcao());
-            List<AvionLeti> avioniDolasci;
-    		try {
-    			avioniDolasci = oSKlijent.getArrivals(ad.getIcao(), vrijemeObrade, vrijemeObrade + preuzimanjeVrijeme);
-    	        if (avioniDolasci != null) {
-    	            System.out.println("Broj letova: " + avioniDolasci.size());
-    	            //TODO upiši podatke u tablicu AERODROMI_DOLASCI
-    	            for(AvionLeti a : avioniDolasci) {
-    	                System.out.println("Avion: " + a.getIcao24() + " Odredište: " + a.getEstDepartureAirport());
-    	            }
-    	        }
-    		} catch (NwtisRestIznimka e) {
-    			e.printStackTrace();
-    		}
-    		//TODO napravi malu pauzu između dva aerodroma
-    		//TODO izračunaj potrebno vrijeme spavanja
-    		long korekcija = 0;
-    		long vrijemeSpavanja = vrijemeCiklusa - korekcija;
-    		try {
+			List<Aerodrom> aerodromi = new ArrayList<>();
+			Aerodrom ad = new Aerodrom("LDZA", "Airport Zagreb", "HR", new Lokacija("0", "0"));
+			aerodromi.add(ad);
+			ad = new Aerodrom("LDVA", "Airport Varaždin", "HR", new Lokacija("0", "0"));
+			aerodromi.add(ad);
+			ad = new Aerodrom("EDDF", "Airport Frankfurt", "DE", new Lokacija("0", "0"));
+			aerodromi.add(ad);
+			ad = new Aerodrom("EDDB", "Airport Berlin", "DE", new Lokacija("0", "0"));
+			aerodromi.add(ad);
+			ad = new Aerodrom("LOWW", "Airport Vienna", "AT", new Lokacija("0", "0"));
+			aerodromi.add(ad);
+
+			for (Aerodrom aerodrom : aerodromi) {
+				List<AvionLeti> avioniPolasci;
+				try {
+					avioniPolasci = oSKlijent.getDepartures(aerodrom.getIcao(), vrijemeObrade,
+							vrijemeObrade + preuzimanjeVrijeme);
+					if (avioniPolasci != null) {
+						System.out.println("Broj letova: " + avioniPolasci.size());
+						// TODO upiši podatke u tablicu AERODROMI_POLASCI
+						for (AvionLeti a : avioniPolasci) {
+							System.out.println("Avion: " + a.getIcao24() + " Odredište: " + a.getEstArrivalAirport());
+						}
+					}
+				} catch (NwtisRestIznimka e) {
+					e.printStackTrace();
+				}
+				System.out.println("Dolasci na aerodrom: " + aerodrom.getIcao());
+				List<AvionLeti> avioniDolasci;
+				try {
+					avioniDolasci = oSKlijent.getArrivals(aerodrom.getIcao(), vrijemeObrade,
+							vrijemeObrade + preuzimanjeVrijeme);
+					if (avioniDolasci != null) {
+						System.out.println("Broj letova: " + avioniDolasci.size());
+						// TODO upiši podatke u tablicu AERODROMI_DOLASCI
+						for (AvionLeti a : avioniDolasci) {
+							System.out.println("Avion: " + a.getIcao24() + " Odredište: " + a.getEstDepartureAirport());
+						}
+					}
+				} catch (NwtisRestIznimka e) {
+					e.printStackTrace();
+				}
+				// TODO napravi malu pauzu između 2 aerodroma
+			}
+			// TODO izračunaj potrebno vrijeme spavanja
+			long korekcija = 0;
+			long vrijemeSpavanja = vrijemeCiklusa - korekcija;
+			try {
 				Thread.sleep(vrijemeSpavanja);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		} 
+		}
 	}
 
 	@Override
--- tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/rest/RestAerodromi.java
+++ tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/rest/RestAerodromi.java
@@ -1,30 +1,59 @@
 package org.foi.nwtis.tskobic.zadaca_2.rest;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("aerodromi")
-@Produces(MediaType.APPLICATION_JSON)
 public class RestAerodromi {
 	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
 	public Response dajSveAerodrome() {
 		Response odgovor = null;
 		return odgovor;
 	}
 	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	@Path("/pracenje")
 	public Response dajAerodromeZaPratiti() {
+		Response odgovor = null;
+		return odgovor;		
+	}
+
+	@POST
+	@Produces({MediaType.APPLICATION_JSON})	
+	public Response dodajAerodromZaPratiti(String icao) {
+		Response odgovor = null;
+		return odgovor;		
+	}
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	@Path("{icao}")
+	public Response dajAerodrom(String icao) {
+		Response odgovor = null;
+		return odgovor;		
+	}
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	@Path("{icao}/polasci")
+	public Response dajPolaskeAerodoma(String icao) {
 		Response odgovor = null;
 		return odgovor;
 	}
 
-	public Response dodajAerodromZaPratiti(String icao) {
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	@Path("{icao}/dolasci")
+	public Response dajDolaskeAerodoma(String icao) 	{
 		Response odgovor = null;
 		return odgovor;
 	}
-	public Response dajAerodrom(String icao) {
-		Response odgovor = null;
-		return odgovor;
-	}
+
 }
