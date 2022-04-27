6a334afa760de769afce0231231abd85bf323912
Toni Škobić <73468642+tskobic@users.noreply.github.com>
2022-04-27 07:06:25 +0000

    27. travnja labosi

--- tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/rest/RestProblemi.java
+++ tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/rest/RestProblemi.java
@@ -1,8 +1,37 @@
 package org.foi.nwtis.tskobic.zadaca_2.rest;
 
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("problemi")
 public class RestProblemi {
-
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response dajSveProbleme() {
+		Response odgovor = null;
+		return odgovor;
+	}
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Path("{icao}")
+	public Response dajProbleme(@PathParam("icao") String icao) {
+		Response odgovor = null;
+		return odgovor;
+	}
+	
+	@DELETE
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Path("{icao}")
+	public Response obrisiProbleme(@PathParam("icao") String icao) {
+		Response odgovor = null;
+		return odgovor;
+	}
 }
---
+++ tskobic-zadaca_2_wa_2/src/main/java/org/foi/nwtis/tskobic/zadaca_2/mvc/PregledAerodroma.java
@@ -0,0 +1,37 @@
+package org.foi.nwtis.tskobic.zadaca_2.mvc;
+
+import java.util.List;
+
+import org.foi.nwtis.podaci.Aerodrom;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.mvc.Controller;
+import jakarta.mvc.Models;
+import jakarta.mvc.View;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Controller @Path("aerodromi")
+@RequestScoped
+
+public class PregledAerodroma {
+	
+	@Inject private Models model;
+	
+	@GET
+	@Path("pocetak")
+	@View("index.jsp")
+	public void pocetak() {
+		
+	}
+	
+	@GET
+	@Path("pregledSvihAerodroma")
+	@View("pregledSvihAerodroma.jsp")
+	public void pregledSvihAerodroma() {
+		AerodromiKlijent ak = new AerodromiKlijent();
+		List<Aerodrom> aerodromi = ak.dajSveAerodrome();
+		model.put("aerodromi", aerodromi);
+	}
+}
