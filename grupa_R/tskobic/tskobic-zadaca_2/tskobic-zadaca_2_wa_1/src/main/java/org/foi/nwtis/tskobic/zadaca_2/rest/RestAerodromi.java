b686e209fe9ab22e4ffeeaa938c91329c18565cf
tskobic <tskobic@58350e7a-5921-49bc-b2bb-43b6036b2832>
2022-04-27 07:06:25 +0000

    27. travnja labosi

--- grupa_R/tskobic/tskobic-zadaca_2/tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/rest/RestAerodromi.java
+++ grupa_R/tskobic/tskobic-zadaca_2/tskobic-zadaca_2_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_2/rest/RestAerodromi.java
@@ -1,47 +1,94 @@
 package org.foi.nwtis.tskobic.zadaca_2.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.foi.nwtis.podaci.Aerodrom;
+import org.foi.nwtis.rest.podaci.Lokacija;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("aerodromi")
 public class RestAerodromi {
-	
+
 	@GET
-	@Produces({MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON })
 	public Response dajSveAerodrome() {
 		Response odgovor = null;
+
+		// TODO preuzmi aerodrome iz tablice AERODROMI_PRACENI
+		List<Aerodrom> aerodromi = new ArrayList<>();
+		Aerodrom ad = new Aerodrom("LDZA", "Airport Zagreb", "HR", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("LDVA", "Airport Varaždin", "HR", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("EDDF", "Airport Frankfurt", "DE", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("EDDB", "Airport Berlin", "DE", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("LOWW", "Airport Vienna", "AT", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+
+		if (aerodromi != null) {
+			odgovor = Response.status(Response.Status.OK).entity(aerodromi).build();
+		} else {
+			odgovor = Response.status(Response.Status.NOT_FOUND).entity("Nema aerodroma.").build();
+		}
+
 		return odgovor;
-	}
-	
-	@GET
-	@Produces({MediaType.APPLICATION_JSON})
-	@Path("/pracenje")
-	public Response dajAerodromeZaPratiti() {
-		Response odgovor = null;
-		return odgovor;		
 	}
 
 	@POST
-	@Produces({MediaType.APPLICATION_JSON})	
+	@Produces({ MediaType.APPLICATION_JSON })
 	public Response dodajAerodromZaPratiti(String icao) {
 		Response odgovor = null;
-		return odgovor;		
+		return odgovor;
 	}
-	
+
 	@GET
-	@Produces({MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON })
 	@Path("{icao}")
-	public Response dajAerodrom(String icao) {
+	public Response dajAerodrom(@PathParam("icao") String icao) {
 		Response odgovor = null;
-		return odgovor;		
+		
+		// TODO preuzmi aerodrome iz tablice AERODROMI_PRACENI
+		List<Aerodrom> aerodromi = new ArrayList<>();
+		Aerodrom ad = new Aerodrom("LDZA", "Airport Zagreb", "HR", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("LDVA", "Airport Varaždin", "HR", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("EDDF", "Airport Frankfurt", "DE", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("EDDB", "Airport Berlin", "DE", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		ad = new Aerodrom("LOWW", "Airport Vienna", "AT", new Lokacija("0", "0"));
+		aerodromi.add(ad);
+		
+		Aerodrom aerodrom = null;
+		for (Aerodrom a : aerodromi) {
+			if(a.getIcao().compareTo(icao) == 0) {
+				aerodrom = a;
+				break;
+			}
+		}
+		
+		if (aerodrom != null) {
+			odgovor = Response.status(Response.Status.OK).entity(aerodrom).build();
+		} else {
+			odgovor = Response.status(Response.Status.NOT_FOUND).entity("Nema aerodroma: " + icao).build();
+		}
+		
+		return odgovor;
 	}
-	
+
 	@GET
-	@Produces({MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON })
 	@Path("{icao}/polasci")
 	public Response dajPolaskeAerodoma(String icao) {
 		Response odgovor = null;
@@ -49,9 +96,9 @@ public class RestAerodromi {
 	}
 
 	@GET
-	@Produces({MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON })
 	@Path("{icao}/dolasci")
-	public Response dajDolaskeAerodoma(String icao) 	{
+	public Response dajDolaskeAerodoma(String icao) {
 		Response odgovor = null;
 		return odgovor;
 	}
---
+++ grupa_R/tskobic/tskobic-zadaca_2/tskobic-zadaca_2_wa_2/src/main/java/org/foi/nwtis/tskobic/zadaca_2/mvc/AerodromiKlijent.java
@@ -0,0 +1,32 @@
+package org.foi.nwtis.tskobic.zadaca_2.mvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.foi.nwtis.podaci.Aerodrom;
+
+import com.google.gson.Gson;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+
+public class AerodromiKlijent {
+
+	public List<Aerodrom> dajSveAerodrome() {
+		Client client = ClientBuilder.newClient();
+		WebTarget webResource = client.target("http://webpredmeti:8080/tskobic-zadaca_2_wa_1/api/aerodromi");
+		Response restOdgovor = webResource.request().header("Accept", "application/json").get();
+		List<Aerodrom> aerodromi = null;
+		if (restOdgovor.getStatus() == 200) {
+			String odgovor = restOdgovor.readEntity(String.class);
+			Gson gson = new Gson();
+			aerodromi = new ArrayList<>();
+			aerodromi.addAll(Arrays.asList(gson.fromJson(odgovor, Aerodrom[].class)));
+		}
+		return aerodromi;
+	}
+
+}
